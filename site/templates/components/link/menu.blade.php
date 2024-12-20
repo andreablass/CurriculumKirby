1f18a223a1956c260867a736bbae5009b339df42
andreablass <andreablass11@gmail.com>
2024-12-20 13:26:47 -0800

    Responsive nav

--- site/templates/components/link/menu.blade.php
+++ site/templates/components/link/menu.blade.php
@@ -1,10 +1,12 @@
-<a href="{{ $href }}" {{ $attributes->merge(['class' => 'text-white  {{-- Establece el color del texto como blanco. --}}
-    no-underline {{-- Elimina el subrayado del enlace. --}}
-    hover:text-pink-400 {{-- Cambia el color del texto a rosa cuando el usuario pasa el ratón sobre el enlace. --}}
-    hover:scale-120 {{-- Aumenta el tamaño del enlace al 120% cuando se pasa el ratón sobre él. --}}
-    hover:font-bold {{-- Hace que el texto se ponga en negrita cuando se pasa el ratón. --}}
-    transition {{-- Aplica una transición suave a los efectos de hover. --}}
-    transform {{-- Permite aplicar transformaciones, como escala o desplazamiento. --}}
-    duration-300']) }}> {{-- Define la duración de la transición en 300 milisegundos. --}}
-        {{ $slot }}
+<a href="{{ $href }}" {{ $attributes->merge(['class' => 'text-white hover:text-pink-400 hover:scale-120 hover:font-bold transition transform duration-300']) }}>
+    {{--
+        - text-white: Establece el color del texto como blanco.
+        - hover:text-pink-400: Cambia el color del texto a rosa cuando el usuario pasa el ratón sobre el enlace.
+        - hover:scale-120: Aumenta el tamaño del enlace al 120% cuando se pasa el ratón sobre él.
+        - hover:font-bold: Hace que el texto se ponga en negrita cuando se pasa el ratón.
+        - transition: Aplica una transición suave a los efectos de hover.
+        - transform: Permite aplicar transformaciones, como escala o desplazamiento.
+        - duration-300: Define la duración de la transición en 300 milisegundos.
+    --}}
+    {{ $slot }}
 </a>
--- site/templates/components/navigation/header.blade.php
+++ site/templates/components/navigation/header.blade.php
@@ -4,26 +4,24 @@
             <x-link.menu href="{{ page('home')->url() }}">Andrea Blass Zamudio</x-link.menu>
         </div>
         <div class="flex flex-1 items-center justify-end gap-4">
-            <nav>
+            <nav class="hidden lg:block">
                 <ul class="flex space-x-6">
+                    @foreach (site()->children()->listed() as $page)
                     <li>
-                        <x-link.menu href="{{ page('contact')->url() }}">Contact</x-link.menu>               
-                    </li>     
+                        <x-link.menu href="{{ $page->url() }}">{{ $page->title() }}</x-link.menu>
+                    </li>
+                    @endforeach
                 </ul>
             </nav>
 
-            <div>
-                <button
-                    class="rounded p-1 text-white hover:bg-gray-700 hover:text-pink-400 "
-                    x-data
-                    @click="$dispatch('menu-open')" 
-                >
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                    </svg>
-                </button>
-            </div>
+            <button 
+                    class="rounded p-1 text-white hover:bg-gray-700 hover:text-pink-400 lg:hidden" 
+                    x-data 
+                    @click="$dispatch('menu-open')">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                </svg>
+            </button>
         </div>
     </div>
 </header>
-
